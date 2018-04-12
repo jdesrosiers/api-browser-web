f414a94683ffecbf4736d42be5e6e473df82a687
Jon Chin <jchin@8thlight.com>
2018-04-12 16:49:47 -0700

    Use array.forEach instead of for / for...of

--- src/link-utils.js
+++ src/link-utils.js
@@ -6,16 +6,16 @@ const parseRawLinks = (rawLinks) => {
   let links = [];
   const splitLinks = rawLinks.split(",");
 
-  for (var i = 0; i < splitLinks.length; i++) {
+  splitLinks.forEach((splitLink) => {
     let link = {};
-    const splitParts = splitLinks[i].split(";");
+    const splitParts = splitLink.split(";");
 
     link.href = splitParts[0].replace(/<(.*)>/, "$1").trim();
     link.rel = splitParts[1].replace(/rel="(.*)"/, "$1").trim();
     link.title = splitParts[2].replace(/title="(.*)"/, "$1").trim();
 
     links.push(link);
-  }
+  });
 
   return links;
 };
@@ -23,11 +23,11 @@ const parseRawLinks = (rawLinks) => {
 const resolveUrls = (links, baseUrl) => {
   const linksCopy = links.slice(0);
 
-  for (let link of linksCopy) {
+  linksCopy.forEach((link) => {
     const fullUrl = new URL(link.href, baseUrl);
     const encodedHref = encodeURIComponent(fullUrl.href);
     link.href = "#" + encodedHref;
-  }
+  });
 
   return linksCopy;
 };
