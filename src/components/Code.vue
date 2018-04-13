affb27ffa09937d22c72148572eb610cc97b6210
Jason Desrosiers <jdesrosi@gmail.com>
2018-04-12 21:52:00 -0700

    Extract Code into it's own component

---
+++ src/components/Code.vue
@@ -0,0 +1,50 @@
+<template>
+  <iframe v-if="language === 'html'" :srcdoc="highlighted"></iframe>
+  <pre v-else-if="language"><code class="hljs" :class="language" v-html="highlighted"></code></pre>
+</template>
+
+<script>
+  import * as HttpParser from "../../lib/parse-http";
+  import hljs from "highlight.js";
+  import "highlight.js/styles/default.css";
+
+  const formatJson = (json) => JSON.stringify(JSON.parse(json), null, "  ");
+
+  export default {
+    name: "Code",
+    props: ["browser"],
+    methods: {
+      highlight(code) {
+        return hljs.highlightAuto(code).value;
+      }
+    },
+    computed: {
+      highlighted() {
+        if (!this.body || this.language === "html") {
+          return this.body;
+        } else if (this.language === "json") {
+          const formatted = formatJson(this.body);
+          return this.highlight(formatted);
+        } else {
+          return this.highlight(this.body);
+        }
+      },
+      language() {
+        return HttpParser.subtypeName(this.browser.headers["content-type"]);
+      },
+      body() {
+        return this.browser.body;
+      }
+    }
+  };
+</script>
+
+<style scoped>
+  pre {
+    margin: 0;
+  }
+
+  iframe {
+    border: none;
+  }
+</style>
--- src/components/Document.vue
+++ src/components/Document.vue
@@ -3,59 +3,28 @@
     <div class="card-header" :class="{ 'bg-danger': statusText }" v-if="statusText">
       {{ statusText }}
     </div>
-    <div v-if="language === 'html'">
-      <iframe :srcdoc="highlighted"></iframe>
-    </div>
-    <pre v-else><code class="hljs" :class="language" v-html="highlighted"></code></pre>
+    <Code :browser="browser" />
     <Link v-for="(link, index) in links" :key="index" :browser="browser" :link="link" />
   </div>
 </template>
 
 <script>
   import * as HttpParser from "../../lib/parse-http";
+  import Code from "@/components/Code.vue";
   import Link from "@/components/Link.vue";
-  import hljs from "highlight.js";
-  import "highlight.js/styles/default.css";
-
-  const formatJson = (json) => JSON.stringify(JSON.parse(json), null, "  ");
 
   export default {
     name: "Document",
     props: ["browser"],
-    components: { Link },
-    data: () => ({ body: "", language: "", links: [] }),
-    mounted() {
-      this.update(this.browser);
-    },
-    methods: {
-      update(browser) {
-        this.language = HttpParser.subtypeName(browser.headers["content-type"]);
-        this.body = browser.body;
-        this.links = browser.headers["link"] ? HttpParser.parseLink(browser.headers["link"]) : [];
-      },
-      highlight(code) {
-        return hljs.highlightAuto(code).value;
-      }
-    },
-    watch: {
-      browser(browser) {
-        this.update(browser);
-      }
-    },
+    components: { Code, Link },
     computed: {
-      highlighted() {
-        if (this.language === "html") {
-          return this.body;
-        } else if (this.language === "json") {
-          const formatted = formatJson(this.body);
-          return this.highlight(formatted);
-        } else {
-          return this.highlight(this.body);
-        }
-      },
       statusText() {
         const status = this.browser.status;
         return status >= 400 && status < 600 ? this.browser.statusText : undefined;
+      },
+      links() {
+        const link = this.browser.headers["link"];
+        return link ? HttpParser.parseLink(link) : [];
       }
     }
   };
@@ -65,13 +34,5 @@
   .card {
     margin-left: 1em;
     margin-right: 1em;
-  }
-
-  pre {
-    margin: 0;
-  }
-
-  iframe {
-    border: none;
   }
 </style>
